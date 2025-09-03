5a92b68a330ca5379c685facad7fa7db98adfa5c
Dotkonnekt <Dotkonnekt@Shabarish>
2025-09-03 14:20:21 +0530

    asaya test suites

--- Test Suites/Asaya.ts
+++ Test Suites/Asaya.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>74aea08e-d768-4aec-91ce-4759007aa64f</testSuiteGuid>
+   <testCaseLink>
+      <guid>1bcfeabd-bd89-402c-854f-82ea04b2dbb0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Asaya</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
