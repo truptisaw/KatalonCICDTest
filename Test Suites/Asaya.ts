4f6230564d279633ac3b29d59ceb6515a12003c0
Dotkonnekt <Dotkonnekt@Shabarish>
2025-09-03 14:17:18 +0530

    Asaya test suit added

---
+++ Test Suites/Asaya.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Asaya</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>74aea08e-d768-4aec-91ce-4759007aa64f</testSuiteGuid>
+</TestSuiteEntity>
